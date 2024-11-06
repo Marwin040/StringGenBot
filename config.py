180e1fd07c2d97b42655ec7ee1f41fa0551b5655
Marwin040 <markusmarwin00@gmail.com>
2024-11-06 12:53:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("26655243"))
-API_HASH = getenv("535a90f092149e8173be6c41a9fd5577")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
-BOT_TOKEN = getenv("7264357275:AAEsKOhuIRMKacIIDFGowJ1PfSkqcAPDlKM")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://marwinalone2002:0PxKXvOj7tByYpoD@cluster0.jmljy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = getenv("BOT_TOKEN")
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 8194005351))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tamilchat_alapparai")
